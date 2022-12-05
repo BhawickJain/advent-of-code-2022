fdec3c7cc23ff44b622db281df137ec5454af7b8
BhawickJain <bhawick@outlook.com>
2022-12-05 15:46:07 +0000

    rename folder to 1 for day-1

--- src/1/compute-puzzle-result.ts
+++ src/1/compute-puzzle-result.ts
@@ -2,7 +2,7 @@ import { getMaxCaloriesFromPuzzleInput } from "./calorie-counting";
 import { readFileSync } from "fs";
 
 async function compute(): Promise<void> {
-  const puzzleInput = readFileSync("./src/day-1/input.txt", "utf-8");
+  const puzzleInput = readFileSync("./src/1/input.txt", "utf-8");
   const puzzleResult = getMaxCaloriesFromPuzzleInput(puzzleInput);
   console.log(puzzleResult);
 }
